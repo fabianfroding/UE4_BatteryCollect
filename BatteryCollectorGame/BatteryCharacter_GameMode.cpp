6d0a9147879bd6622d3c3fa543e93e2d7d2dbaba
fabian <fabian.froding@gmail.com>
2022-03-26 17:43:54 +0100

    add random spawn timer

--- BatteryCollectorGame/BatteryCharacter_GameMode.cpp
+++ BatteryCollectorGame/BatteryCharacter_GameMode.cpp
@@ -2,4 +2,36 @@
 
 
 #include "BatteryCharacter_GameMode.h"
+#include "GameFramework/Actor.h"
 
+
+ABatteryCharacter_GameMode::ABatteryCharacter_GameMode()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ABatteryCharacter_GameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FTimerHandle UnusedHandle;
+	GetWorldTimerManager().SetTimer(
+		UnusedHandle, this, &ABatteryCharacter_GameMode::SpawnPlayerRecharge,
+		FMath::RandRange(2.0f, 5.0f), true);
+}
+
+void ABatteryCharacter_GameMode::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ABatteryCharacter_GameMode::SpawnPlayerRecharge()
+{
+	float RandX = FMath::RandRange(Spawn_X_MIN, Spawn_X_MAX);
+	float RandY = FMath::RandRange(Spawn_Y_MIN, Spawn_Y_MAX);
+
+	FVector SpawnPosition = FVector(RandX, RandY, Spawn_Z);
+	FRotator SpawnRotation = FRotator(0.0f, 0.0f, 0.0f);
+
+	GetWorld()->SpawnActor(PlayerRecharge, &SpawnPosition, &SpawnRotation);
+}
--- BatteryCollectorGame/BatteryCharacter_GameMode.h
+++ BatteryCollectorGame/BatteryCharacter_GameMode.h
@@ -13,5 +13,28 @@ UCLASS()
 class BATTERYCOLLECTORGAME_API ABatteryCharacter_GameMode : public AGameMode
 {
 	GENERATED_BODY()
+
+	ABatteryCharacter_GameMode();
+
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere)
+		TSubclassOf<APawn> PlayerRecharge;
+
+	float Spawn_Z = 500.0f;
+
+	UPROPERTY(EditAnywhere)
+		float Spawn_X_MIN;
+
+	UPROPERTY(EditAnywhere)
+		float Spawn_X_MAX;
+
+	UPROPERTY(EditAnywhere)
+		float Spawn_Y_MIN;
+
+	UPROPERTY(EditAnywhere)
+		float Spawn_Y_MAX;
 	
+	void SpawnPlayerRecharge();
 };
